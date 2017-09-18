ff2a01404759bdb7cfcd887b0d47a993a6738a39
crocandr <slezsak.andras@gmail.com>
2017-09-18 12:13:18 +0000

    mail.rb - ruby based mail sender tool added

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
 FROM ubuntu:xenial
 
-RUN apt-get update && apt-get install -y net-tools less curl vim tar gzip openvpn easy-rsa libpam-google-authenticator libpam-radius-auth freeradius-utils 
+RUN apt-get update && apt-get install -y net-tools less curl vim tar gzip ruby openvpn easy-rsa libpam-google-authenticator libpam-radius-auth freeradius-utils 
 
 COPY files/template-client.ovpn /etc/
 COPY files/openvpn-vars /etc/
-COPY files/generate-newclient-cert.sh /opt/
-COPY files/revoke-client-cert.sh /opt/
-COPY files/list-old-keys.sh /opt/
+COPY files/*.sh /opt/
+COPY files/tools /opt/
 # Radius conf preparation
 COPY files/pam_openvpn /etc/ 
 # scripts - permission change
---
+++ files/tools/mail.rb
@@ -0,0 +1,54 @@
+require 'net/smtp'
+require 'optparse'
+
+def_server = "192.168.10.254"
+def_subject = "VPN Notice"
+def_from = "noreply@mycompany.com"
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: <this program.rb> [options]"
+
+  opts.on('-x', '--server SERVER', 'address of the smtp server') { |server| options[:server] = server }
+  opts.on('-t', '--mailto EMAILADDRESS', 'mail recipient') { |mailto| options[:mailto] = mailto }
+  opts.on('-s', '--subject TEXT', 'subject of the mail') { |subject| options[:subject] = subject }
+  opts.on('-f', '--from EMAILADDRESS', 'from address of the mail') { |from| options[:from] = from }
+  opts.on('-m', '--message TEXT', 'message' ) { |message| options[:message] = message }
+
+  opts.on('-h', '--help', 'display help') { puts opts }
+
+end.parse!
+
+#server ==  nil ? ( server = def_server )
+#subject == nil ? ( subject = def_subject )
+
+options[:server] ? ( server = options[:server] ) : ( server = def_server )
+options[:subject] ? ( subject = options[:subject] ) : ( subject = def_subject )
+options[:from] ? ( from = options[:from] ) : ( from = def_from )
+if options[:mailto] == nil
+    puts "No recipient defined"
+    exit
+  else
+    mailto = options[:mailto]
+end
+if options[:message] == nil
+    puts "No message defined"
+    exit
+  else
+    message = options[:message]
+end
+
+#message = <<MESSAGE_END
+#From: System <noreply@sonrisa.hu> 
+#Subject: options[:subject]
+
+#options[:message]
+#
+#teszt teszt teszt
+#MESSAGE_END
+
+mailmsg="Subject: " + subject + "\n" + message
+
+Net::SMTP.start(server) do |smtp|
+  smtp.send_message mailmsg, from , mailto
+end
